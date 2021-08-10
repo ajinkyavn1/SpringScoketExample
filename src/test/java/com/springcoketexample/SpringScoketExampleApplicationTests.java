8308b6f2e0cbb44be11877179d8e5751ce4d9515
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-08-10 17:40:26 +0530

    Create SpringScoketExampleApplicationTests.java

---
+++ src/test/java/com/springcoketexample/SpringScoketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcoketexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScoketExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
