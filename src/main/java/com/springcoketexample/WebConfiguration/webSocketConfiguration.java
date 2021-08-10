6daff751a88b8bfcc741b67c304750ca19473cb9
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-08-10 23:28:22 +0530

    Configured

---
+++ src/main/java/com/springcoketexample/WebConfiguration/webSocketConfiguration.java
@@ -0,0 +1,23 @@
+package com.springcoketexample.WebConfiguration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class webSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app")
+        .enableSimpleBroker("/topic");
+    }
+}
