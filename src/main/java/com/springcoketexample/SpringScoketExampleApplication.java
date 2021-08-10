72ba73c778497ced4d945c0894d576b88d26814e
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-08-10 17:40:15 +0530

    Create SpringScoketExampleApplication.java

---
+++ src/main/java/com/springcoketexample/SpringScoketExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springcoketexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScoketExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScoketExampleApplication.class, args);
+    }
+
+}
